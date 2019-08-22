fdc5847be12188e4d59c010070b673a825ad3405
Yandi <yandikurniawan@gmail.com>
2019-08-22 21:34:52 +0700

    Implement redis cache

--- server.js
+++ server.js
@@ -9,6 +9,41 @@ const client = redis.createClient(REDIS_PORT)
 
 const app = express()
 
+function setResponse(username, repos) {
+    return `<h2>${username} has ${repos} repository</h2>`
+}
+
+async function getRepos(req, res, next) {
+    try {
+        console.log('Fetch data')
+        const { username } = req.params
+        const response = await fetch(`https://api.github.com/users/${username}`)
+        const data = await response.json()
+        const repos = data.public_repos
+        client.setex(username, 3600, repos)
+        res.send(setResponse(username, repos))
+    } catch (error) {
+        console.error(error)
+        res.status(500)
+    }
+}
+
+function cache(req, res, next) {
+    const {username } = req.params
+
+    client.get(username, (err, data) => {
+        if(err) throw err
+
+        if(data !== null){
+            res.send(setResponse(username, data))
+        }else{
+            next()
+        }
+    })
+}
+
+app.get('/repos/:username', cache, getRepos)
+
 app.listen(5000, () => {
     console.log(`listening on port ${PORT}`)
 })
